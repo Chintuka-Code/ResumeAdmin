72273924a99870a228578b48f5a93a1961647b6c
SabbanData <sabban@datatrained>
2021-03-01 18:21:56 +0530

    'Setup'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginGuard } from './Gaurd/login.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    canActivate: [LoginGuard],
+    loadChildren: () =>
+      import('./Module/login/login.module').then((m) => m.LoginModule),
+  },
+  {
+    path: 'main',
+    loadChildren: () =>
+      import('./Module/main/main.module').then((m) => m.MainModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
